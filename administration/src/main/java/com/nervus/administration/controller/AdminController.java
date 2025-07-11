aef363769af91d70258f59a3769561619f8b6cac
nervus-tech <nervus.tech@gmail.com>
2025-07-11 13:04:32 +0300

    Add test endpoint to AdminController

--- administration/src/main/java/com/nervus/administration/controller/AdminController.java
+++ administration/src/main/java/com/nervus/administration/controller/AdminController.java
@@ -1,5 +1,12 @@
 package com.nervus.administration.controller;
 
-public class AdminController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class AdminController {
+    @GetMapping("/test")
+    public String test() {
+        return "Administration Service is running";
+    }
 }
