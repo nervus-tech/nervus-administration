308912a9401cb1e4d53717cc38b6421cd8f3745c
nervus-tech <nervus.tech@gmail.com>
2025-08-05 11:51:53 +0300

    Add Spring Cloud 2025.0.0 support and Eureka client configuration for Administration service

--- administration/src/main/java/com/nervus/administration/AdministrationApplication.java
+++ administration/src/main/java/com/nervus/administration/AdministrationApplication.java
@@ -2,8 +2,10 @@ package com.nervus.administration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdministrationApplication {
 
 	public static void main(String[] args) {
